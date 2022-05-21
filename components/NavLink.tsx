ff30411b06f70e4b52745655f7ed07c028b00875
dangust <dangust.moonboy@gmail.com>
2022-05-21 17:53:19 +0700

    fix type error

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -1,3 +1,4 @@
+import { LinkProps } from 'next/link'
 import { useRouter } from 'next/router'
 import PropTypes from 'prop-types'
 import { Link } from '.'
@@ -13,9 +14,14 @@ NavLink.defaultProps = {
   exact: false,
 }
 
-function NavLink({ children, href, exact, ...props }) {
+function NavLink({
+  children,
+  href,
+  exact,
+  ...props
+}: LinkProps & { children: string; exact?: boolean; className?: string }) {
   const { pathname } = useRouter()
-  const isActive = exact ? pathname === href : pathname.startsWith(href)
+  const isActive = exact ? pathname === href : pathname.startsWith(href as string)
 
   if (isActive) {
     props.className += ' active'
